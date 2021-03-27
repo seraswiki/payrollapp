6a75a94801d2032cb90c8442f40e2b52ec2664e0
seras <seraswiki@gmail.com>
2021-03-27 12:33:03 +0800

    rename test class

--- src/test/java/com/example/payroll/PayrollApplicationIntegrationTests.java
+++ src/test/java/com/example/payroll/PayrollApplicationIntegrationTests.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class PayrollApplicationTests {
+class PayrollApplicationIntegrationTests {
 	@Autowired
 	private MockMvc mvc;
 
